5f00b1e64c15484137777295ae7b856b5e861f54
mayukh18 <mayukh.superb@gmail.com>
2016-10-04 01:45:38 +0530

    new

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='reco',
-      version='0.1',
+      version='0.1.1',
       description='a simple yet powerful recommendation systems library in python',
       url='http://github.com/mayukh18/reco',
       author='Mayukh Bhattacharyya',
       author_email='mayukh.superb@gmail.com',
       license='MIT',
-      download_url = 'https://github.com/mayukh18/reco/tarball/0.1.0',
+      download_url = 'https://github.com/mayukh18/reco/tarball/0.1.1',
       keywords=['recommendation'],
       packages=['reco'],
       install_requires=['numpy',
